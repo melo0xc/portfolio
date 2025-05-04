5981dacabad2a684b84537e0039fdee0cd29bf48
melo-zip <matheus.webp@gmail.com>
2025-05-04 08:17:21 -0300

    refactor: next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    appDir: true
+  },
+  srcDir: 'src'
 };
 
 export default nextConfig;
